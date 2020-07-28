e5a648048617e98087150b2d874ee514007c5d19
Vishwas <gopinav5@gmail.com>
2020-07-28 20:09:56 +0530

    useState behaviour

--- react-render-demo/src/components/UseState/UseState.js
+++ react-render-demo/src/components/UseState/UseState.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 
 export const UseState = () => {
   const [count, setCount] = useState(0)
-  
+
   console.log('UseState Render')
   return (
     <div>
       <button onClick={() => setCount(c => c + 1)}>Count - {count}</button>
+      <button onClick={() => setCount(0)}>Count to 0</button>
+      <button onClick={() => setCount(5)}>Count to 5</button>
     </div>
   )
 }
